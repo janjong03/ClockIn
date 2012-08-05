bf40079e281bedc1173b820e2b2790ef2ed739db
Edward Paulo Ganuelas <epganuelas@gmail.com>
2012-08-05 19:03:13 -0400

    Added new activities, images

--- gen/com/mustadio98/clockin/R.java
+++ gen/com/mustadio98/clockin/R.java
@@ -13,14 +13,20 @@ public final class R {
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int logo=0x7f020002;
+        public static final int start=0x7f020003;
     }
     public static final class id {
         public static final int LinearLayout1=0x7f070000;
-        public static final int clock_in=0x7f070002;
-        public static final int menu_settings=0x7f070001;
+        public static final int RelativeLayout1=0x7f070001;
+        public static final int clock_in=0x7f070005;
+        public static final int imageView1=0x7f070002;
+        public static final int menu_settings=0x7f070004;
+        public static final int startBtn=0x7f070003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int start_layout=0x7f030001;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
---
+++ src/com/mustadio98/clockin/Start.java
@@ -0,0 +1,33 @@
+package com.mustadio98.clockin;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class Start extends Activity implements OnClickListener {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_layout);
+        View startButton = findViewById(R.id.startBtn);
+        startButton.setOnClickListener(this);
+     
+    }
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		Intent i;
+		switch (v.getId()) {
+		case R.id.startBtn:
+			i= new Intent(this, MainActivity.class);
+			startActivity(i);
+			break;
+		}
+		
+	}
+
+}
