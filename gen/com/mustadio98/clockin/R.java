97ceac61b178060547652ebada9f5bfd760fb5ca
Edward Ganuelas <epganuelas@gmail.com>
2012-08-08 17:43:58 -0400

    Added tablet layout

--- gen/com/mustadio98/clockin/R.java
+++ gen/com/mustadio98/clockin/R.java
@@ -14,7 +14,9 @@ public final class R {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
         public static final int logo=0x7f020002;
-        public static final int start=0x7f020003;
+        public static final int logo2=0x7f020003;
+        public static final int start=0x7f020004;
+        public static final int start2=0x7f020005;
     }
     public static final class id {
         public static final int LinearLayout1=0x7f070000;
