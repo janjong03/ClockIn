63d66f919c26a94befd9454bee0bbfe75b460410
Edward Ganuelas <epganuelas@gmail.com>
2012-08-10 17:07:35 -0400

    color scheme

--- gen/com/mustadio98/clockin/R.java
+++ gen/com/mustadio98/clockin/R.java
@@ -10,6 +10,10 @@ package com.mustadio98.clockin;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int Background=0x7f040000;
+        public static final int Button_Shade=0x7f040001;
+    }
     public static final class drawable {
         public static final int breaks=0x7f020000;
         public static final int exit=0x7f020001;
@@ -24,42 +28,42 @@ public final class R {
         public static final int stop=0x7f02000a;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f070000;
-        public static final int RelativeLayout1=0x7f070007;
-        public static final int breakout=0x7f07000c;
-        public static final int clock_in=0x7f07000a;
-        public static final int clockout=0x7f07000b;
-        public static final int imageView1=0x7f070008;
-        public static final int menu_settings=0x7f07000d;
-        public static final int startBtn=0x7f070009;
-        public static final int textView1=0x7f070001;
-        public static final int textView2=0x7f070003;
-        public static final int textView3=0x7f070005;
-        public static final int txtEndTime=0x7f070004;
-        public static final int txtStartTime=0x7f070002;
-        public static final int txtTotal=0x7f070006;
+        public static final int LinearLayout1=0x7f080000;
+        public static final int RelativeLayout1=0x7f080007;
+        public static final int breakout=0x7f08000c;
+        public static final int clock_in=0x7f08000a;
+        public static final int clockout=0x7f08000b;
+        public static final int imageView1=0x7f080008;
+        public static final int menu_settings=0x7f08000d;
+        public static final int startBtn=0x7f080009;
+        public static final int textView1=0x7f080001;
+        public static final int textView2=0x7f080003;
+        public static final int textView3=0x7f080005;
+        public static final int txtEndTime=0x7f080004;
+        public static final int txtStartTime=0x7f080002;
+        public static final int txtTotal=0x7f080006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int start_layout=0x7f030001;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_main=0x7f070000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int break_in=0x7f040005;
-        public static final int break_out=0x7f040006;
-        public static final int break_time=0x7f04000a;
-        public static final int clock_in=0x7f040003;
-        public static final int clock_out=0x7f040004;
-        public static final int end_time=0x7f040008;
-        public static final int menu_settings=0x7f040001;
-        public static final int start_time=0x7f040007;
-        public static final int title_activity_main=0x7f040002;
-        public static final int work_time=0x7f040009;
+        public static final int app_name=0x7f050000;
+        public static final int break_in=0x7f050005;
+        public static final int break_out=0x7f050006;
+        public static final int break_time=0x7f05000a;
+        public static final int clock_in=0x7f050003;
+        public static final int clock_out=0x7f050004;
+        public static final int end_time=0x7f050008;
+        public static final int menu_settings=0x7f050001;
+        public static final int start_time=0x7f050007;
+        public static final int title_activity_main=0x7f050002;
+        public static final int work_time=0x7f050009;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f060000;
     }
 }
