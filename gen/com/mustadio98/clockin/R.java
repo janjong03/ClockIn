1c7c26e1b8b630f3ab1fb29e380afb2aa192a068
Edward Ganuelas <epganuelas@gmail.com>
2012-08-09 23:34:52 -0400

    Added Menu icons

--- gen/com/mustadio98/clockin/R.java
+++ gen/com/mustadio98/clockin/R.java
@@ -11,12 +11,17 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int logo=0x7f020002;
-        public static final int logo2=0x7f020003;
-        public static final int start=0x7f020004;
-        public static final int start2=0x7f020005;
+        public static final int breaks=0x7f020000;
+        public static final int exit=0x7f020001;
+        public static final int ic_action_search=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int logo=0x7f020004;
+        public static final int logo2=0x7f020005;
+        public static final int settings=0x7f020006;
+        public static final int start=0x7f020007;
+        public static final int start2=0x7f020008;
+        public static final int startico=0x7f020009;
+        public static final int stop=0x7f02000a;
     }
     public static final class id {
         public static final int LinearLayout1=0x7f070000;
